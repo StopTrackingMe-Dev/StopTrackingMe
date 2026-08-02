55563a02066ef700a983f7bd018be09766bf76d1
jyeric <jyeric1@126.com>
2026-08-03 06:25:26 +0800

    Bump app version to 0.1.1-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "app.stoptrackingme"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.0-alpha"
+        versionCode = 6
+        versionName = "0.1.1-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
