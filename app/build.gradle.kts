44ef124bf2e8d8c5430be4900b37bfaad9a2a3ba
jyeric <jyeric1@126.com>
2026-08-03 02:24:27 +0800

    Set app version to 0.1.0-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "app.stoptrackingme"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.0.4-alpha"
+        versionCode = 5
+        versionName = "0.1.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
